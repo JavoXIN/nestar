85135a2d35a5e626062147bbfd2e30f1ffb1c791
Javoxir <javohir.toshtemirov123@gmail.com>
2025-06-08 17:26:32 +0900

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,20 @@
+export enum Message {
+    SOMETHING_WENT_WRONG = "Something went wrong!",
+    NO_DATA_FOUND = "No data is found!",
+    CREATE_FAILED = "Create is failed!",
+    UPDATE_FAILED = "Update is failed!",
+    REMOVE_FAILED = "Remove is failed!",
+    UPLOAD_FAILED = "Upload is failed!",
+    BAD_REQUEST = "Bad request!",
+
+
+    NO_MEMBER_NICK = "You have not set your nickname yet! Please set it first!",
+    BLOCKED_USER = "You have been blocked! Contact to restaurant!",
+    WRONG_PASSWORD = "Wrong password, plaese try again!",
+    NOT_AUTHENTICATED = "You are not authenticated! Please login first!",
+    TOKEN_NOT_EXIST = "Token does not exist!",
+    ONLY_SPECIFIC_ROLES_ALLOWED = "Only specific roles are allowed to access this resource!",
+    NOT_ALLOWED_REQUEST = "Not allowed request!",
+    PROVIDE_ALLOWED_FORMAT = "Please provide allowed format!",
+    SELF_SUBCRIPTION_DENIED = "You cannot subscribe to yourself!",
+}
