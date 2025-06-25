a9f534fb4e90936731e95da62794be0771f2e8cc
Javoxir <javohir.toshtemirov123@gmail.com>
2025-06-25 18:55:51 +0900

    feat: develop getProperties GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -4,6 +4,16 @@ import { ObjectId } from "bson";
 export const availableAgentSorts = ['createdAt','updatetedAt','memberLikes','memberViews','memberRank'];
 export const availableMemberSorts = ['createdAt','updatetedAt','memberLikes','memberViews'];
 
+export const availableOptions = ['propertyBarter', 'propertyRent'];
+export const availablePropertySorts = [
+	'createdAt',
+	'updatedAt',
+	'propertyLikes',
+	'propertyViews',
+	'propertyRank',
+	'propertyPrice',
+];
+
  /** IMAGE CONFIGURATION (config.js) */
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
@@ -19,3 +29,11 @@ export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === 'string' ? new ObjectId(target) : target;
 };
 
+export const lookupMember = {
+	$lookup: {
+		from: 'members',
+		localField: 'memberId',
+		foreignField: '_id',
+		as: 'memberData',
+	},
+};
