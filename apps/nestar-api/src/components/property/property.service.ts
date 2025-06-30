ca561489c1153fe3f173f30e5045507ef868eebd
Javoxir <javohir.toshtemirov123@gmail.com>
2025-06-30 15:41:54 +0900

    feat: develop updatePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -269,4 +269,43 @@ export class PropertyService {
 
         return result[0];
     }
+
+
+
+
+    public async updatePropertyByAdmin(input: PropertyUpdate): Promise<Property> {
+        
+        let {propertyStatus, soldAt, deletedAt} = input;
+        
+        const search: T = {
+            _id: input._id,
+            propertyStatus: PropertyStatus.ACTIVE,
+        };
+
+        if(propertyStatus === PropertyStatus.SOLD) input.soldAt = moment().toDate();
+        else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+        //const updatedProperty: Property = await this.propertyModel
+        const result = await this.propertyModel
+            .findOneAndUpdate(search, input, {new: true})
+            .exec();
+
+        //if(!updatedProperty) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+        if(!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+        
+        if(soldAt || deletedAt) {
+            await this.memberService.memberStatsEditor({
+                _id: result.memberId,
+                targetKey: 'memberProperties',
+                modifier: -1,
+            });
+        }
+
+        return result;
+    }
+
+
+
+
+
 }
