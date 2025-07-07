dcf662a7d19aba5e6866118068c0645a99fa008a
Javoxir <javohir.toshtemirov123@gmail.com>
2025-07-07 10:30:54 +0900

    feat: develop likeTargetProperty GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId } from 'mongoose';
+import { Model, ObjectId, Schema } from 'mongoose';
 import { Properties, Property } from '../../libs/dto/property/property';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { AgentPropertiesInquiry, AllPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
@@ -13,6 +13,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import * as moment from 'moment';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 
 @Injectable()
@@ -21,6 +24,7 @@ export class PropertyService {
         @InjectModel('Property') private readonly propertyModel: Model<Property>,
         private memberService: MemberService,
         private viewService: ViewService,
+        private likeService: LikeService,
     ) {}
 
     public async createProperty(input: PropertyInput): Promise<Property> {
@@ -236,6 +240,33 @@ export class PropertyService {
         if(!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
         return result[0];
+    }
+
+
+    public async likeTargetProperty(memberId: ObjectId, likeRefId: ObjectId): Promise<Property> {
+        const target: Property | null = await this.propertyModel
+            .findOne({_id: likeRefId, propertyStatus: PropertyStatus.ACTIVE})
+            .exec();
+        
+        if(!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        const input: LikeInput = {
+            memberId: memberId,
+            likeRefId: likeRefId,
+            likeGroup: LikeGroup.PROPERTY,
+        };
+
+        // Like Toggle via Like modules
+        const modifier: number = await this.likeService.toggleLike(input);
+
+        const result = await this.propertyStatsEditor({
+            _id: likeRefId,
+            targetKey: 'propertyLikes',
+            modifier: modifier
+        });
+
+        if(!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+        return result;
     }
 
 
